9fd1581070ec2904dafd558620c5cb5cb5f60bd5
JordieRayanne <119185845+JordieRayanne@users.noreply.github.com>
2023-04-07 20:46:56 +0300

    HashMap data.send recoverable data

--- Framework/etu1954/framework/Modelview.java
+++ Framework/etu1954/framework/Modelview.java
@@ -1,11 +1,30 @@
 package etu1954.framework;
 
+import java.util.HashMap;
+
 /**
  *
  * @author ETU1954
  */
 public class Modelview {
     String view;
+    HashMap<String, Object> data;
+
+    public Modelview() {
+        this.data = new HashMap<>();
+    }
+
+    public void addItem(String key, Object value) {
+        this.data.put(key, value);
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
 
     public String getView() {
         return view;
