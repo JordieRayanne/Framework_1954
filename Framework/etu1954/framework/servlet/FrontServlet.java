381539607a3c8369d7b10f4066605900407f6e84
JordieRayanne <119185845+JordieRayanne@users.noreply.github.com>
2023-04-18 21:15:19 +0300

    compare attributes and parameter names

--- Framework/etu1954/framework/servlet/FrontServlet.java
+++ Framework/etu1954/framework/servlet/FrontServlet.java
@@ -4,8 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -68,25 +74,101 @@ public class FrontServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
         try {
-            PrintWriter out = response.getWriter();
             String current = request.getRequestURI().replace(request.getContextPath(), "");
             response.getWriter().println("Current URI: " + current); // Debugging
 
             if (mappingUrls.containsKey(current)) {
                 Mapping mapp = mappingUrls.get(current);
                 Object result = executeMethod(mapp.getClassName(), mapp.getMethod());
-                if (result instanceof Modelview) {
-                    Modelview model = (Modelview) result;
-                    forwardToView(request, response, model);
+
+                String saveUrl = "/" + mapp.getClassName() + "-save";
+
+                if (current.compareTo(saveUrl) == 0) {
+                    // get parameterNames
+                    ArrayList<String> names = new ArrayList<String>();
+                    Enumeration<String> paramNames = request.getParameterNames();
+                    while (paramNames.hasMoreElements()) {
+                        String paramName = (String) paramNames.nextElement();
+                        names.add(paramName);
+                    }
+
+                    String packag = "etu1954.framework.models.";
+                    String classname = mapp.getClassName();
+                    Class<?> clazz = null;
+                    clazz = Class.forName(packag + classname);
+                    Object obj = clazz.getConstructor().newInstance();
+
+                    if (clazz != null) {
+                        Field[] fields = clazz.getDeclaredFields();
+
+                        setMatchingFieldValue(request, response, names, fields, obj);
+
+                        // appeler la methode save
+                        Method saveMethod = obj.getClass().getMethod("save");
+                        saveMethod.invoke(obj);
+                    }
+
+                    // return model view
+                    else if (result instanceof Modelview) {
+                        Modelview model = (Modelview) result;
+                        forwardToView(request, response, model);
+                    } else {
+                        out.println("Error: The method did not return a Modelview.");
+                    }
+
                 } else {
-                    out.println("Error: The method did not return a Modelview.");
+                    out.println("Error: the url is false");
                 }
+
+            } else {
+                out.println("Error:");
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            response.getWriter().println("Une erreur est survenue : " + e.getMessage());
+            e.printStackTrace(out);
+            // response.getWriter().println("Une erreur est survenue : " + e.getMessage());
         }
+    }
+
+    // set the value of a field if match with parameter name
+    public static void setMatchingFieldValue(HttpServletRequest request, HttpServletResponse response,
+            ArrayList<String> parameterName, Field[] fields, Object obj) throws ServletException, Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        for (String paramName : parameterName) {
+            for (Field field : fields) {
+                // out.println(paramName + "..." + field);
+                if (compare(paramName, field)) {
+                    String paramValue = request.getParameter(paramName);
+                    field.setAccessible(true);
+
+                    if (field.getType() == String.class) {
+                        field.set(obj, paramValue);
+                        out.println(field.getName() + ":" + field.get(obj));
+                    } else if (field.getType() == int.class || field.getType() == Integer.class) {
+                        field.set(obj, Integer.parseInt(paramValue));
+                        out.println(field.getName() + ":" + field.get(obj));
+                    } else if (field.getType() == double.class || field.getType() == Double.class) {
+                        field.set(obj, Double.parseDouble(paramValue));
+                        out.println(field.getName() + ":" + field.get(obj));
+                    } else if (field.getType() == Date.class) {
+                        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                        Date daty = (Date) format.parse(paramValue);
+                        field.set(obj, daty);
+                        out.println(field.getName() + ":" + field.get(obj));
+                    }
+                }
+            }
+        }
+    }
+
+    // compare two string
+    public static boolean compare(String parameterName, Field field) {
+        if (field.getName().equals(parameterName)) {
+            return true;
+        }
+        return false;
     }
 
     private Object executeMethod(String className, String methodName) throws Exception {
--- Test_framework/etu1954/framework/models/Myclass.java
+++ Test_framework/etu1954/framework/models/Myclass.java
@@ -1,13 +1,21 @@
 package etu1954.framework.models;
 
+import java.util.Date;
+
 import etu1954.framework.Modelview;
 import etu1954.framework.annotation.MyUrl;
+import etu1954.framework.annotation.MyUrl.MyURL;
 
 /**
  *
  * @author ETU1954
  */
 public class Myclass {
+    String nom;
+    int age;
+    Double poids;
+    Date date;
+
     @MyUrl.MyURL(url = "/my-url")
     public Modelview myMethod() {
         Modelview view = new Modelview();
@@ -19,5 +27,42 @@ public class Myclass {
             e.printStackTrace();
         }
         return view;
+    }
+
+    @MyURL(url = "/Myclass-save")
+    public void save() {
+        System.out.println("Nom:" + this.getNom() + "age:" + this.getAge());
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Double getPoids() {
+        return poids;
+    }
+
+    public void setPoids(Double poids) {
+        this.poids = poids;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
